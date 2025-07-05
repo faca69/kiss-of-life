8c1b1eddd322fd8428a11c882d96518e1a354f3a
faca69 <fnikolov123456789@gmail.com>
2025-07-05 23:26:21 +0200

    added grass texture

--- client/src/components/Terrain.tsx
+++ client/src/components/Terrain.tsx
@@ -1,14 +1,21 @@
 "use client";
 
+import { useTexture } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
 import React from "react";
+import * as THREE from "three";
 
 function Terrain({ position }: { position: [number, number, number] }) {
+  const grassColorMap = useTexture("/textures/grass.jpg");
+
+  grassColorMap.wrapS = grassColorMap.wrapT = THREE.RepeatWrapping;
+  grassColorMap.repeat.set(22, 22);
+
   return (
     <RigidBody type="fixed" friction={0.5} name="terrain">
       <mesh receiveShadow position={position}>
         <boxGeometry args={[100, 2, 100]} />
-        <meshStandardMaterial color="green" />
+        <meshStandardMaterial map={grassColorMap} />
       </mesh>
     </RigidBody>
   );
